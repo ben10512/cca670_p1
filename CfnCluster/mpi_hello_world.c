9bb6eb3b8d409e12f9cf90a2659385fe31bcbd25
Ben Deibert <backpackingat@gmail.com>
2019-04-03 22:46:01 -0400

    Adding Source files to GitHub.

---
+++ CfnCluster/mpi_hello_world.c
@@ -0,0 +1,12 @@
+/*A Parallel Hello World Program*/
+#include <stdio.h>
+#include <mpi.h>
+main(int argc, char **argv)
+{
+  int a, node;
+  MPI_Comm_rank(MPI_COMM_WORLD, &node);
+  for ( a = 1; a < 5; a=a+1){
+    printf("Hello World from Node %d\n",node);
+  }
+  MPI_Finalize();
+}
