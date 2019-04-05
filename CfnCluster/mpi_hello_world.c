8195bf3c782d2589752ecdcbc5efd524f0e4ca76
Ben Deibert <backpackingat@gmail.com>
2019-04-04 20:20:36 -0400

    Adding Init line

--- CfnCluster/mpi_hello_world.c
+++ CfnCluster/mpi_hello_world.c
@@ -4,6 +4,7 @@
 main(int argc, char **argv)
 {
   int a, node;
+  MPI_Init(&argc,&argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &node);
   for ( a = 1; a < 5; a=a+1){
     printf("Hello World from Node %d\n",node);
